77651db4a6140c3e14e947bf513cba66af293aad
spartshubs-arch <spartshubs@gmail.com>
2026-01-31 11:46:03 +0500

    Update adminSliderRoutes.js

--- routes/adminSliderRoutes.js
+++ routes/adminSliderRoutes.js
@@ -1,3 +1,25 @@
+// const express = require('express');
+// const router = express.Router();
+// const uploadCloud = require('../config/cloudinary');
+
+// const {
+//   getSliders,
+//   uploadSlider,
+//   updateSlider,
+//   deleteSlider
+// } = require('../controllers/adminSliderController');
+
+// router.get('/sliders', getSliders);
+// router.post('/slider',uploadCloud.single('image'), uploadSlider);
+// router.put('/slider/:id', uploadCloud.single('image'),updateSlider);
+// router.delete('/slider/:id', deleteSlider);
+
+
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const router = express.Router();
 const uploadCloud = require('../config/cloudinary');
@@ -6,13 +28,19 @@ const {
   getSliders,
   uploadSlider,
   updateSlider,
-  deleteSlider
+  deleteSlider,
+   getCloudinarySignature
 } = require('../controllers/adminSliderController');
 
 router.get('/sliders', getSliders);
-router.post('/slider',uploadCloud.single('image'), uploadSlider);
-router.put('/slider/:id', uploadCloud.single('image'),updateSlider);
+router.get("/cloudinary-signature", getCloudinarySignature);
+// router.post('/slider',uploadCloud.single('image'), uploadSlider);
+// router.put('/slider/:id', uploadCloud.single('image'),updateSlider);
+router.post('/slider', uploadSlider);
+router.put('/slider/:id', updateSlider);
 router.delete('/slider/:id', deleteSlider);
+
+
 
 
 
