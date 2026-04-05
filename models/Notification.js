523f0bfcf4d1a51f7ee82fe655ab4542a81364c8
spartshubs-arch <spartshubs@gmail.com>
2026-04-06 02:08:48 +0500

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -1,28 +1,47 @@
-// models/Notification.js
 
 const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
   {
+    recipientType: {
+      type: String,
+      enum: ["admin", "vendor", "user"],
+      required: true,
+    },
+
+    recipientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "recipientModel",
+    },
+
+    recipientModel: {
+      type: String,
+      enum: ["Admin", "Vendor", "User"],
+      required: true,
+    },
+
+    senderType: {
+      type: String,
+      enum: ["superadmin", "system"],
+      default: "superadmin",
+    },
+
     subject: {
       type: String,
       required: true,
       trim: true,
     },
+
     message: {
       type: String,
       required: true,
       trim: true,
     },
-    targetType: {
-      type: String,
-      enum: ["admin", "vendor", "both"],
-      required: true,
-    },
-    readBy: {
-      // stores IDs of admins/vendors who have read it
-      type: [String],
-      default: [],
+
+    isRead: {
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true }
